c41c03ef1096de197eb0d614712350737e1b35e4
Shipachev Gleb <90782197+Shpchv@users.noreply.github.com>
2021-09-15 19:10:32 +0300

    adding homework
    Simple mathematics

---
+++ Homework.py
@@ -0,0 +1,15 @@
+print('Какое бы число Вы ни ввели, ряд арифметических операций превратит в конце это число в 4')
+number = int(input())
+a = number
+number *= 2
+print(number, '- результат умножения на 2')
+number += 8
+print(number, '- прибавили 8')
+number /= 2
+print(number, '- результат деления на 2')
+number -= a
+print(number, '- будум тсс')
+
+
+
+
